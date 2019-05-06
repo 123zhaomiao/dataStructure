bca0b222f1448768006a011cf55919860df984d3
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-06 17:52:05 +0800

    test.c

--- 逆波兰表达式/test.c
+++ 逆波兰表达式/test.c
@@ -8,9 +8,9 @@ void TestRPN()
 	operate  cal[] = { { DATA, 12 }, { DATA, 3 }, { DATA, 4 }, { ADD, 0 }, { MUL, 0 },
 	{ DATA, 6 }, { SUB, 0 }, { DATA, 8 }, { DATA, 2 }, { DIV, 0 }, { ADD, 0 } };
 	size = sizeof(cal) / sizeof(cal[0]);
-	//ջ��ʼ��
+	//栈初始化
 	StackInit(&s);
-	//�沨������ʽ�����
+	//逆波兰表达式的求解
 	ret = CalcRPN(cal,&s,size);
 	printf("ret = %d\n", ret);
 }
