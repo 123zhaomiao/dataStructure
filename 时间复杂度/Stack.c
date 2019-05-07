a33d4d30be74df49652e68424adaca217327bfd6
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-07 18:45:56 +0800

    Stack.c

--- 时间复杂度/Stack.c
+++ 时间复杂度/Stack.c
@@ -1,7 +1,7 @@
 #include"Stack.h"
 
 
-//��ʼ��
+//初始化
 void InitStack(stack *ps)
 {
 	assert(ps);
@@ -9,39 +9,39 @@ void InitStack(stack *ps)
 	memset(ps->data, 0, Max*sizeof(DataType));
 }
 
-//ջΪ��
+//栈为空
 int StackEmpty(stack *ps)
 {
 	assert(ps);
 	return ps->top == 0;
 }
-//ջ��Ԫ��
+//栈顶元素
 DataType StackTop(stack *ps)
 {
 	assert(ps);
 	return ps->data[ps->top - 1];
 }
 
-//��ջ
+//入栈
 void PushStack(stack *s, stack *mins, DataType d)
 {
 	assert(s);
 	assert(mins);
 	s->data[s->top++] = d;
-	//����СֵջΪ�գ�����Ҫ��������ݱ���Сֵ��ջ��Ԫ��С����Ҳ����Сֵջ
+	//若最小值栈为空，或者要插入的数据比最小值的栈顶元素小，则也入最小值栈
 	if (StackEmpty(mins)||d <= StackTop(mins))
 	{
 		mins->data[mins->top++] = d;
 	}
 }
-//ջ��Сֵ
+//栈最小值
 DataType MinStack(stack *mins)
 {
 	assert(mins);
 	return mins->data[mins->top - 1];
 }
 
-//��ջ
+//出栈
 void PopStack(stack *s, stack *mins)
 {
 	assert(s);
